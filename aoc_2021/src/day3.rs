18f4567114a9b1383e151e543ab5fa3e4c901de9
Jobin Nelson <jobinnelson369@gmail.com>
2022-09-27 13:06:57 +0530

    completed 2021 day3

--- aoc_2021/src/day3.rs
+++ aoc_2021/src/day3.rs
@@ -24,11 +24,40 @@ pub fn run() {
         .into_iter()
         .map(|d| if d > half { 1 } else { 0 })
         .fold(0, |acc, digit| (acc << 1) + digit);
+    let epsilon: u32 = !gamma << size - digits >> size - digits;
 
-    println!("Part 1: {}", gamma * (!gamma << size - digits >> size - digits));
-    println!("Part 2: {}", part_two());
-}
+    let filter_vals = |f: fn(u32, u32) -> bool| {
+        let mut values = lines.clone();
+        for idx in 0..digits {
+            let line_values: Vec<_> = values.iter().map(|digits| digits[idx]).collect();
 
-fn part_two() -> usize {
-    todo!();
+            let oxygen_half = (line_values.len() as u32 + 1) / 2;
+            let oxygen_sum = line_values.iter().sum::<u32>();
+            let most_common = if oxygen_sum < oxygen_half { 0 } else { 1 };
+
+            values = values
+                .into_iter()
+                .filter(|value| f(value[idx], most_common))
+                .collect();
+
+            if values.len() == 1 {
+                break;
+            }
+        }
+        values
+    };
+
+    let oxygen_values = filter_vals(|digit, most_common| digit == most_common);
+    let co2_values = filter_vals(|digit, most_common| digit != most_common);
+
+    let oxygen_value = oxygen_values[0]
+        .iter()
+        .fold(0, |res, digit| (res << 1) + digit);
+
+    let co2_value = co2_values[0]
+        .iter()
+        .fold(0, |res, digit| (res << 1) + digit);
+
+    println!("Part 1: {}", gamma * epsilon);
+    println!("Part 2: {}", oxygen_value * co2_value);
 }
