fd7e493832a4d6633d98c31e1921fdc5bcb41a66
Jobin Nelson <jobinnelson369@gmail.com>
2022-12-11 15:03:38 +0530

    2022 day 6 done

---
+++ aoc_2022/src/bin/day06.rs
@@ -0,0 +1,19 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = std::fs::read_to_string("data/day06.txt").unwrap();
+    let data_stream: Vec<char> = input.chars().collect();
+
+    println!("Part 1: {}", find_marker(&data_stream, 4).unwrap());
+    println!("Part 2: {}", find_marker(&data_stream, 14).unwrap());
+}
+
+fn find_marker(stream: &Vec<char>, distinct_num: usize) -> Option<usize> {
+    for (i, w) in stream.windows(distinct_num).enumerate() {
+        let unique: HashSet<&char> = HashSet::from_iter(w);
+        if unique.len() == distinct_num {
+            return Some(i + distinct_num);
+        }
+    }
+    None
+}
